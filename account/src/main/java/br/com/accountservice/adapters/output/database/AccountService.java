b22c8cbe7733647b06ef4f0c30b6df55e50175a6
Emm4nuell <eduardo.eesf@gmail.com>
2024-07-24 12:49:30 -0300

    Add save Account

--- account/src/main/java/br/com/accountservice/adapters/output/database/AccountService.java
+++ account/src/main/java/br/com/accountservice/adapters/output/database/AccountService.java
@@ -1,15 +1,22 @@
 package br.com.accountservice.adapters.output.database;
 
 import br.com.accountservice.adapters.output.database.entity.AccountEntity;
+import br.com.accountservice.adapters.output.database.repository.IAccountRepository;
 import br.com.accountservice.application.domain.model.AccountModel;
 import br.com.accountservice.application.ports.out.ICreateAccountService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class AccountService implements ICreateAccountService {
 
+    private final IAccountRepository iAccountRepository;
+
+    @Transactional
     @Override
     public void create(AccountModel accountModel) {
         var entity = AccountEntity.builder()
@@ -21,6 +28,7 @@ public class AccountService implements ICreateAccountService {
                 .account_status(accountModel.getAccount_status())
                 .account_data(accountModel.getAccount_data())
                 .build();
-        log.info("Service adapters --> " + entity.toString());
+        iAccountRepository.save(entity);
+        log.info("Account save success --> " + entity.toString());
     }
 }
--- account/src/main/java/br/com/accountservice/adapters/output/database/entity/AccountEntity.java
+++ account/src/main/java/br/com/accountservice/adapters/output/database/entity/AccountEntity.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@Table(name = "account")
+@Table(name = "accounts")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
